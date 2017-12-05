d9603c05d591b9f1f1b4b3ac281be3cf08ba7fb6
Unknown <eduardofuentesfierro@gmail.com>
2017-12-05 15:54:34 -0300

    Testing an error

---
+++ Assets/Scripts/Deck Builder/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck {
+
+	public List<Card> deck;
+	public int deckID;
+	public string deckName;
+
+	public Deck(List<Card> _deck, int _deckID, string _deckName){
+		deck = _deck;
+		deckID = _deckID;
+		deckName = _deckName;
+	}
+
+	void AddCard(Card card){
+
+		deck.Add (card);
+
+	}
+
+
+	void RemoveCard(Card card){
+		deck.Remove (card);
+	}
+}
